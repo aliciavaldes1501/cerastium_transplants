b275a6a2b9c8ff2048374cc04407bff94cf92e6c
aliciavaldes1501 <aliciavaldes1501@gmail.com>
2025-06-09 17:04:02 +0200

    Modifications before submitting to GCB: change calculation of temperature, modify gitignore

--- try_maps_plots_plants.R
+++ try_maps_plots_plants.R
@@ -7,7 +7,10 @@ ggplot(data=data_transplants%>%filter(year==2022&plot=="H04"&!is.na(temp)),
                         limits=range(data_transplants$temp, na.rm = TRUE))+
   scale_fill_distiller(palette="RdYlBu",
                        limits=range(data_transplants$temp_diff, na.rm = TRUE))+
-  coord_fixed()+theme(legend.position="top")
+  coord_fixed()+theme(legend.position="top")+ggtitle("H04")+
+  theme(plot.title = element_text(hjust = 0.01, vjust = -22))
+ggsave(file="output/figures/map_H04.tiff", width=11,height=15,dpi=300,
+       compression="lzw")
 
 ggplot(data=data_transplants%>%filter(year==2022&plot=="H05"&!is.na(temp)),
        aes(x=x,y=y,color=temp,fill=temp_diff,label=unique_id))+
@@ -16,7 +19,8 @@ ggplot(data=data_transplants%>%filter(year==2022&plot=="H05"&!is.na(temp)),
                         limits=range(data_transplants$temp, na.rm = TRUE))+
   scale_fill_distiller(palette="RdYlBu",
                        limits=range(data_transplants$temp_diff, na.rm = TRUE))+
-  coord_fixed()+theme(legend.position="top")
+  coord_fixed()+theme(legend.position="top")+ggtitle("H05")+
+  theme(plot.title = element_text(hjust = 0.01, vjust = -22))
 
 ggplot(data=data_transplants%>%filter(year==2022&plot=="H08"&!is.na(temp)),
        aes(x=x,y=y,color=temp,fill=temp_diff,label=unique_id))+
@@ -53,6 +57,8 @@ ggplot(data=data_transplants%>%filter(year==2022&plot=="HC1"&!is.na(temp)),
   scale_fill_distiller(palette="RdYlBu",
                        limits=range(data_transplants$temp_diff, na.rm = TRUE))+
   coord_fixed()+theme(legend.position="top")
+ggsave(file="output/figures/map_HC1.tiff", width=11,height=15,dpi=300,
+       compression="lzw")
 
 ggplot(data=data_transplants%>%filter(year==2022&plot=="HC4"&!is.na(temp)),
        aes(x=x,y=y,color=temp,fill=temp_diff,label=unique_id))+
@@ -80,6 +86,6 @@ ggplot(data=data_transplants%>%filter(year==2022&!is.na(temp)),
   scale_color_distiller(palette="YlOrRd",direction=1)+
   scale_fill_distiller(palette="RdYlBu")+
   theme(legend.position="top")+
-  facet_wrap(~plot,ncol=2,scales="free")
+  facet_wrap(~plot,ncol=4)+coord_fixed()
 ggsave(file="output/figures/map_plots.tiff", width=11,height=15,dpi=300,
        compression="lzw")
